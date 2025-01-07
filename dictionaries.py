925c4519c33c256ab5f3e4eeaaea597ac49b424c
kartheek119-git <dandasaikartheek150@gmail.com>
2025-01-07 12:37:19 +0530

    Add files via upload

---
+++ dictionaries.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+dict={"a":"hello","b":"Sai","c":"kartheek"}
+print(dict["a"])
+print(dict["b"])
+print(dict["c"])
+dict
+
+
+# In[7]:
+
+
+dict["d"]=5
+
+
+# In[8]:
+
+
+print(dict)
+
+
+# In[9]:
+
+
+del dict["d"]
+
+
+# In[11]:
+
+
+print(dict)
+
+
+# In[28]:
+
+
+dictionary={
+    "my_dict":{"a":"sai","b":"Kartheek","role":"student"},
+    "my_dict1":{"a":"narendra","b":"varma","role":"developer"},
+}
+print(dictionary["my_dict1"]["a"])
+print(dictionary["my_dict1"]["b"])
+print(dictionary["my_dict1"]["role"])
+
+
+# In[ ]:
+
+
+
+
